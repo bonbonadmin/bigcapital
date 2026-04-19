50fc9f1697b352867002c51196dbd92562e31fbe
bonbonadmin <admin@bonbon.co.id>
2026-04-19 22:46:46 +0700

    various

--- packages/server/src/modules/WithholdingTaxes/WithholdingTaxes.module.ts
+++ packages/server/src/modules/WithholdingTaxes/WithholdingTaxes.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TenancyContext } from '../Tenancy/TenancyContext.service';
+import { RegisterTenancyModel } from '../Tenancy/TenancyModels/Tenancy.module';
 import { TransformerInjectable } from '../Transformer/TransformerInjectable.service';
 import { CreateWithholdingTaxService } from './commands/CreateWithholdingTax.service';
 import { DeleteWithholdingTaxService } from './commands/DeleteWithholdingTax.service';
 import { EditWithholdingTaxService } from './commands/EditWithholdingTax.service';
 import { WithholdingTaxesController } from './WithholdingTaxes.controller';
 import { WithholdingTaxesApplication } from './WithholdingTaxesApplication.service';
+import { WithholdingTax } from './models/WithholdingTax.model';
 import { GetWithholdingTaxService } from './queries/GetWithholdingTax.service';
 import { GetWithholdingTaxesService } from './queries/GetWithholdingTaxes.service';
 
+const models = [RegisterTenancyModel(WithholdingTax)];
+
 @Module({
+  imports: [...models],
   controllers: [WithholdingTaxesController],
   providers: [
     TransformerInjectable,
